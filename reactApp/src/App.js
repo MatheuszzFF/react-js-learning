676fbbcf92baa1ab622a08342719c55d0ff038a2
mzmfelix <matheus.flaviano@mzgroup.com>
2023-03-21 19:17:31 -0300

    curso react origamid

---
+++ reactApp/src/App.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+import React from 'react';
+import './App.css'
+const App = () => {
+    return (
+        <a href="#">Clique aqui!!!</a>
+    )
+}
+
+export default App;
