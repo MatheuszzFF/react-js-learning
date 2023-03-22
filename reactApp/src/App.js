979654067f065549aef9d48a458083349dd070ea
mzmfelix <matheus.flaviano@mzgroup.com>
2023-03-22 10:37:45 -0300

    curso react origamid

--- reactApp/src/App.js
+++ reactApp/src/App.js
@@ -1,9 +1,27 @@
 import ReactDOM from 'react-dom'
 import React from 'react';
 import './App.css'
+
+const Button = () => {
+    const [counter, setCounter] = React.useState(0);
+    function comprar() {
+        setCounter(counter + 1);
+    }
+
+    return (
+        <div>
+            <button onClick={comprar}>Comprar</button>
+            <p>Quantidade: {counter}</p>
+            <p>Preço:  R$ {counter * 250}</p>
+        </div>
+    )
+}
 const App = () => {
     return (
-        <a href="#">Clique aqui!!!</a>
+        <div>
+            <a href="#">Clique aqui!!!</a>
+            <Button />
+        </div>
     )
 }
 
